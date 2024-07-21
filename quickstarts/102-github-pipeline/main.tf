95969bbfa937345e53e46f19c43f141e591d8081
MarvinBangert <50577590+MarvinBangert@users.noreply.github.com>
2024-07-22 01:54:28 +0200

    Update main.tf
    Signed-off-by: MarvinBangert <50577590+MarvinBangert@users.noreply.github.com>

--- quickstarts/102-github-pipeline/main.tf
+++ quickstarts/102-github-pipeline/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     powerplatform = {
       source  = "microsoft/power-platform"
-      version = "~> 2.6.1-preview"
+      version = "2.6.1-preview"
     }
   }
 
