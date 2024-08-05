b66b36a10fb2fe034d89514ef061990d8f083920
Marvin Bangert <marvin.bangert@glueckkanja.com>
2024-08-05 20:39:00 +0200

    Added another environment

--- quickstarts/102-github-pipeline/main.tf
+++ quickstarts/102-github-pipeline/main.tf
@@ -28,3 +28,13 @@ provider "powerplatform" {
    }
  }
 
+ resource "powerplatform_environment" "dev" {
+   location          = "europe"
+   display_name      = "github-terraform-test2"
+   environment_type  = "Sandbox"
+   dataverse = {
+    language_code     = 1033
+    currency_code     = "EUR"
+    security_group_id = "00000000-0000-0000-0000-000000000000"
+   }
+ }
